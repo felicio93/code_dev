a6c0b5310f0aabdcd847a7c13d7656afdf2b8541
Felicio Cassalho <1620665785113305@mil>
2024-10-18 15:41:08 -0400

    added artic and leo sat

--- Fig_STOFS_Pac.py
+++ Fig_STOFS_Pac.py
@@ -52,7 +52,16 @@ def concat_stofs_pac(start_date, end_date, averaged=True):
                 ),
                 chunks={},
                 engine='h5netcdf',
-                drop_variables=['vvel4.5','uvel4.5','vvel_bottom','uvel_bottom','vvel_surface','uvel_surface','salt_bottom','temp_bottom','depth']
+                drop_variables=['vvel4.5',
+                                'uvel4.5',
+                                'vvel_bottom',
+                                'uvel_bottom',
+                                'vvel_surface',
+                                'uvel_surface',
+                                'salt_bottom',
+                                'temp_bottom',
+                                # 'depth',
+                                ]
             )
             
             ds2 = xr.open_dataset(
@@ -61,7 +70,16 @@ def concat_stofs_pac(start_date, end_date, averaged=True):
                 ),
                 chunks={},
                 engine='h5netcdf',
-                drop_variables=['vvel4.5','uvel4.5','vvel_bottom','uvel_bottom','vvel_surface','uvel_surface','salt_bottom','temp_bottom','depth']
+                drop_variables=['vvel4.5',
+                                'uvel4.5',
+                                'vvel_bottom',
+                                'uvel_bottom',
+                                'vvel_surface',
+                                'uvel_surface',
+                                'salt_bottom',
+                                'temp_bottom',
+                                # 'depth',
+                                ]
             )
         
             ds = xr.concat([ds1,ds2],dim="time",data_vars="all")
@@ -77,7 +95,16 @@ def concat_stofs_pac(start_date, end_date, averaged=True):
                 ),
                 chunks={},
                 engine='h5netcdf',
-                drop_variables=['vvel4.5','uvel4.5','vvel_bottom','uvel_bottom','vvel_surface','uvel_surface','salt_bottom','temp_bottom','depth']
+                drop_variables=['vvel4.5',
+                                'uvel4.5',
+                                'vvel_bottom',
+                                'uvel_bottom',
+                                'vvel_surface',
+                                'uvel_surface',
+                                'salt_bottom',
+                                'temp_bottom',
+                                # 'depth'
+                                ]
             )
             if averaged==True:
                 ds = ds.mean(dim='time')
@@ -125,85 +152,32 @@ def fixed_connectivity_tri():
 
     return x,y,connect_tri
 
-def plot_stofs_pac_T(
+def plot_stofs_pac(
                triangulation,
                z,
+               depth=None,
+               isobaths=None,
                output_dir="",
                n=0,
                long_name="name of the var",
                time_str="time str",
-               vmin1=0,
-               vmax1=32,
-               latmin2=45,
-               latmax2=67,
-               lonmin2=170,
-               lonmax2=200,
-               vmin2=5,
-               vmax2=15,
-               latmin3=42,
-               latmax3=47,
-               lonmin3=233,
-               lonmax3=238,
-               vmin3=10,
-               vmax3=20,
-               interv=0.5):
+               vmin1=None,
+               vmax1=None,
+               latmin2=None,
+               latmax2=None,
+               lonmin2=None,
+               lonmax2=None,
+               vmin2=None,
+               vmax2=None,
+               latmin3=None,
+               latmax3=None,
+               lonmin3=None,
+               lonmax3=None,
+               vmin3=None,
+               vmax3=None,
+               interv=None):
     
-    fig = plt.figure(figsize=(10,9))
-    ax = fig.add_subplot(212)
-    tp = ax.tripcolor(triangulation, z,shading='gouraud',cmap='jet', vmin=vmin1, vmax=vmax1)#'gouraud' = linearly interp. 'flat'=averaged to tri
-    div = make_axes_locatable(ax)
-    cax = div.append_axes("right", size="5%", pad=0.2)
-    cb = fig.colorbar(tp, cax=cax, extend='both',boundaries=np.arange(vmin1,vmax1,interv))
-    cb.set_label(long_name)
-    rect1 = patches.Rectangle((lonmin2, latmin2), lonmax2-lonmin2, latmax2-latmin2, linewidth=1, edgecolor='k', facecolor='none')
-    rect2 = patches.Rectangle((lonmin3, latmin3), lonmax3-lonmin3, latmax3-latmin3, linewidth=1, edgecolor='k', facecolor='none')
-    ax.add_patch(rect1)
-    ax.add_patch(rect2)
-    ax.set_title(time_str)
-    
-    ax2 = fig.add_subplot(221)
-    ax2.set_xlim(lonmin2,lonmax2)
-    ax2.set_ylim(latmin2,latmax2)
-    tp2 = ax2.tripcolor(triangulation, z,shading='gouraud',cmap='jet', vmin=vmin2, vmax=vmax2)
-    div2 = make_axes_locatable(ax2)
-    cax2 = div2.append_axes("right", size="5%", pad=0.2)
-    cb2 = fig.colorbar(tp2, cax=cax2, extend='both',boundaries=np.arange(vmin2,vmax2,interv))
-    
-    ax3 = fig.add_subplot(222)
-    ax3.set_xlim(lonmin3,lonmax3)
-    ax3.set_ylim(latmin3,latmax3)
-    tp3 = ax3.tripcolor(triangulation, z,shading='gouraud',cmap='jet', vmin=vmin3, vmax=vmax3)
-    div3 = make_axes_locatable(ax3)
-    cax3 = div3.append_axes("right", size="5%", pad=0.2)
-    cb3 = fig.colorbar(tp3, cax=cax3, extend='both',boundaries=np.arange(vmin3,vmax3,interv))
-    
-    fig.tight_layout()
-    fig.savefig(output_dir+'{:04d}.jpeg'.format(n))
-    plt.clf()
 
-def plot_stofs_pac_S(
-               triangulation,
-               z,
-               output_dir="",
-               n=0,
-               long_name="name of the var",
-               time_str="time str",
-               vmin1=30,
-               vmax1=37,
-               latmin2=45,
-               latmax2=67,
-               lonmin2=170,
-               lonmax2=200,
-               vmin2=28,
-               vmax2=34.5,
-               latmin3=42,
-               latmax3=47,
-               lonmin3=233,
-               lonmax3=238,
-               vmin3=27,
-               vmax3=33.5,
-               interv=0.5):
-    
     fig = plt.figure(figsize=(10,9))
     ax = fig.add_subplot(212)
     tp = ax.tripcolor(triangulation, z,shading='gouraud',cmap='jet', vmin=vmin1, vmax=vmax1)#'gouraud' = linearly interp. 'flat'=averaged to tri
@@ -216,6 +190,7 @@ def plot_stofs_pac_S(
     ax.add_patch(rect1)
     ax.add_patch(rect2)
     ax.set_title(time_str)
+    ax.set_facecolor('grey')
     
     ax2 = fig.add_subplot(221)
     ax2.set_xlim(lonmin2,lonmax2)
@@ -224,6 +199,7 @@ def plot_stofs_pac_S(
     div2 = make_axes_locatable(ax2)
     cax2 = div2.append_axes("right", size="5%", pad=0.2)
     cb2 = fig.colorbar(tp2, cax=cax2, extend='both',boundaries=np.arange(vmin2,vmax2,interv))
+    ax2.set_facecolor('grey')
     
     ax3 = fig.add_subplot(222)
     ax3.set_xlim(lonmin3,lonmax3)
@@ -232,74 +208,24 @@ def plot_stofs_pac_S(
     div3 = make_axes_locatable(ax3)
     cax3 = div3.append_axes("right", size="5%", pad=0.2)
     cb3 = fig.colorbar(tp3, cax=cax3, extend='both',boundaries=np.arange(vmin3,vmax3,interv))
-    
+    ax3.set_facecolor('grey')
+
+    if depth is not None and isobaths is not None:
+        depth[np.isnan(depth)] = -9999
+        ax2.tricontour(triangulation, depth, levels=isobaths, linewidths=.75, colors="k")
+        ax3.tricontour(triangulation, depth, levels=isobaths, linewidths=.75, colors="k")
+
     fig.tight_layout()
+
     if isinstance(n, str):
         fig.savefig(output_dir+'{}.jpeg'.format(n))
     else:
         fig.savefig(output_dir+'{:04d}.jpeg'.format(n))
+
+    # fig.savefig(output_dir+'{:04d}.jpeg'.format(n))
     plt.clf()
 
-def plot_stofs_pac_H(
-               triangulation,
-               z,
-               output_dir="",
-               n=0,
-               long_name="name of the var",
-               time_str="time str",
-               vmin1=-2.0,
-               vmax1=2.0,
-               latmin2=45,
-               latmax2=67,
-               lonmin2=170,
-               lonmax2=200,
-               vmin2=-1,#5,
-               vmax2=1,#15,
-               latmin3=42,
-               latmax3=47,
-               lonmin3=233,
-               lonmax3=238,
-               vmin3=-1.5,
-               vmax3=1.5,
-               interv=0.1):
-    
-    fig = plt.figure(figsize=(10,9))
-    ax = fig.add_subplot(212)
-    tp = ax.tripcolor(triangulation, z,shading='gouraud',cmap='jet', vmin=vmin1, vmax=vmax1)#'gouraud' = linearly interp. 'flat'=averaged to tri
-    div = make_axes_locatable(ax)
-    cax = div.append_axes("right", size="5%", pad=0.2)
-    cb = fig.colorbar(tp, cax=cax, extend='both',boundaries=np.arange(vmin1,vmax1,interv))
-    cb.set_label(long_name)
-    rect1 = patches.Rectangle((lonmin2, latmin2), lonmax2-lonmin2, latmax2-latmin2, linewidth=1, edgecolor='k', facecolor='none')
-    rect2 = patches.Rectangle((lonmin3, latmin3), lonmax3-lonmin3, latmax3-latmin3, linewidth=1, edgecolor='k', facecolor='none')
-    ax.add_patch(rect1)
-    ax.add_patch(rect2)
-    ax.set_title(time_str)
-    
-    ax2 = fig.add_subplot(221)
-    ax2.set_xlim(lonmin2,lonmax2)
-    ax2.set_ylim(latmin2,latmax2)
-    tp2 = ax2.tripcolor(triangulation, z,shading='gouraud',cmap='jet', vmin=vmin2, vmax=vmax2)
-    div2 = make_axes_locatable(ax2)
-    cax2 = div2.append_axes("right", size="5%", pad=0.2)
-    cb2 = fig.colorbar(tp2, cax=cax2, extend='both',boundaries=np.arange(vmin2,vmax2,interv))
-    
-    ax3 = fig.add_subplot(222)
-    ax3.set_xlim(lonmin3,lonmax3)
-    ax3.set_ylim(latmin3,latmax3)
-    tp3 = ax3.tripcolor(triangulation, z,shading='gouraud',cmap='jet', vmin=vmin3, vmax=vmax3)
-    div3 = make_axes_locatable(ax3)
-    cax3 = div3.append_axes("right", size="5%", pad=0.2)
-    cb3 = fig.colorbar(tp3, cax=cax3, extend='both',boundaries=np.arange(vmin3,vmax3,interv))
-    
-    fig.tight_layout()
-    if isinstance(n, str):
-        fig.savefig(output_dir+'{}.jpeg'.format(n))
-    else:
-        fig.savefig(output_dir+'{:04d}.jpeg'.format(n))
-    plt.clf()
-
-def main(var,start_date,end_date,averaged,output_dir,timestep):
+def main(var,start_date,end_date,averaged,output_dir,timestep,isobaths):
 
     ds = concat_stofs_pac(start_date,end_date,averaged=averaged)
     if var == 'temp_surface':
@@ -312,39 +238,97 @@ def main(var,start_date,end_date,averaged,output_dir,timestep):
         times=[str(tt).split('.')[0] for tt in np.array(ds['time'][:])]
     else:
         times=f"average from {start_date} to {end_date}"
+
+    if isobaths is not None and averaged == False:
+        depth=np.array(ds.variables['depth'][0])
+    if isobaths is not None and averaged == True:
+        depth=np.array(ds.variables['depth'])
+    else:
+        depth=None
+
     ds = np.array(ds.variables[var])
+    print("here is depth:",depth)
     x,y,connect_tri = fixed_connectivity_tri()
     triangulation = tri.Triangulation(x=x, y=y, triangles=connect_tri)
     if averaged==False:
-        for n,z in enumerate(ds):
-            if n>0 and timestep>0:
-                n=(n*timestep)-1
+        for n in range(0, len(ds), timestep):
             print(n)
-            #z = np.array(ds.variables[var][n])
+            z = ds[n]
             time_str=times[n]
             if var == 'temp_surface':
-                plot_stofs_pac_T(triangulation,
+                plot_stofs_pac(triangulation,
                             z,
+                            depth=depth,
+                            isobaths=isobaths,
                             output_dir=output_dir,
                             n=n,
                             long_name=long_name,
                             time_str=time_str,
+                            vmin1=0,
+                            vmax1=32,
+                            latmin2=45,
+                            latmax2=67,
+                            lonmin2=170,
+                            lonmax2=200,
+                            vmin2=5,
+                            vmax2=15,
+                            latmin3=42,
+                            latmax3=47,
+                            lonmin3=233,
+                            lonmax3=238,
+                            vmin3=10,
+                            vmax3=20,
+                            interv=0.5,
                 )
             if var == 'salt_surface':
-                plot_stofs_pac_S(triangulation,
+                plot_stofs_pac(triangulation,
                             z,
+                            depth=depth,
+                            isobaths=isobaths,
                             output_dir=output_dir,
                             n=n,
                             long_name=long_name,
                             time_str=time_str,
+                            vmin1=30,
+                            vmax1=37,
+                            latmin2=45,
+                            latmax2=67,
+                            lonmin2=170,
+                            lonmax2=200,
+                            vmin2=28,
+                            vmax2=34.5,
+                            latmin3=42,
+                            latmax3=47,
+                            lonmin3=233,
+                            lonmax3=238,
+                            vmin3=27,
+                            vmax3=33.5,
+                            interv=0.5,
                 )
             if var == 'elev':
-                plot_stofs_pac_H(triangulation,
+                plot_stofs_pac(triangulation,
                             z,
+                            depth=depth,
+                            isobaths=isobaths,
                             output_dir=output_dir,
                             n=n,
                             long_name=long_name,
                             time_str=time_str,
+                            vmin1=-2.0,
+                            vmax1=2.0,
+                            latmin2=45,
+                            latmax2=67,
+                            lonmin2=170,
+                            lonmax2=200,
+                            vmin2=-1,#5,
+                            vmax2=1,#15,
+                            latmin3=42,
+                            latmax3=47,
+                            lonmin3=233,
+                            lonmax3=238,
+                            vmin3=-1.5,
+                            vmax3=1.5,
+                            interv=0.1,
                 )
         files = os.listdir(output_dir)
         images = []
@@ -356,40 +340,92 @@ def main(var,start_date,end_date,averaged,output_dir,timestep):
     else:
         z=ds
         if var == 'temp_surface':
-            plot_stofs_pac_T(triangulation,
+            plot_stofs_pac(triangulation,
                         z,
+                        depth=depth,
+                        isobaths=isobaths,
                         output_dir=output_dir,
                         n="average_temp_surface",
                         long_name=long_name,
                         time_str=times,
+                        vmin1=0,
+                        vmax1=32,
+                        latmin2=45,
+                        latmax2=67,
+                        lonmin2=170,
+                        lonmax2=200,
+                        vmin2=5,
+                        vmax2=15,
+                        latmin3=42,
+                        latmax3=47,
+                        lonmin3=233,
+                        lonmax3=238,
+                        vmin3=10,
+                        vmax3=20,
+                        interv=0.5,
             )
         if var == 'salt_surface':
-            plot_stofs_pac_S(triangulation,
+            plot_stofs_pac(triangulation,
                         z,
+                        depth=depth,
+                        isobaths=isobaths,
                         output_dir=output_dir,
                         n="average_salt_surface",
                         long_name=long_name,
                         time_str=times,
+                        vmin1=30,
+                        vmax1=37,
+                        latmin2=45,
+                        latmax2=67,
+                        lonmin2=170,
+                        lonmax2=200,
+                        vmin2=28,
+                        vmax2=34.5,
+                        latmin3=42,
+                        latmax3=47,
+                        lonmin3=233,
+                        lonmax3=238,
+                        vmin3=27,
+                        vmax3=33.5,
+                        interv=0.5,
             )
         if var == 'elev':
-            plot_stofs_pac_S(triangulation,
+            plot_stofs_pac(triangulation,
                         z,
+                        depth=depth,
+                        isobaths=isobaths,
                         output_dir=output_dir,
                         n="average_height_surface",
                         long_name=long_name,
                         time_str=times,
+                        vmin1=-2.0,
+                        vmax1=2.0,
+                        latmin2=45,
+                        latmax2=67,
+                        lonmin2=170,
+                        lonmax2=200,
+                        vmin2=-1,#5,
+                        vmax2=1,#15,
+                        latmin3=42,
+                        latmax3=47,
+                        lonmin3=233,
+                        lonmax3=238,
+                        vmin3=-1.5,
+                        vmax3=1.5,
+                        interv=0.1,
             )
 
 
 if __name__ == '__main__':
 
     # Enter your inputs here:
-    var = 'elev'#'salt_surface'#'temp_surface'
+    var = 'temp_surface'#'elev'#'salt_surface'#'temp_surface'
     start_date='20240801'
-    end_date='20240901'
-    averaged=False #if true it calculates the climatology, if false it will be one file per timestep
-    output_dir=r"C:\Users\Felicio.Cassalho\Work\Modeling\AK_Project\STOFS_postprocessing\figures\2024_08_ssh/"
+    end_date='20240801'
+    isobaths=[200,2000]#None
+    averaged=True #if True it calculates the climatology, if False it will be one file per timestep
+    output_dir=r"C:\Users\Felicio.Cassalho\Work\Modeling\AK_Project\STOFS_postprocessing\figures\2024_08_sst/"
     #output_dir=r"/work2/noaa/nosofs/felicioc/work/stofs_pac/figures/sst_2024_08/"
     timestep=6 #every X hours
 
-    main(var,start_date,end_date,averaged,output_dir,timestep)
+    main(var,start_date,end_date,averaged,output_dir,timestep,isobaths)
